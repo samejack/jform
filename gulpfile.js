d24372af4a7848b31a8c9a150da9d2142db1d238
SJ Chou <sj@toright.com>
2017-11-22 20:11:00 +0800

    source first commit

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var mkdirp = require('mkdirp');
+var webpack = require('webpack');
+var uglify = require('uglify-js');
+
+var NAME = 'form-kit';
+var ENTRY = './src/form-kit.js';
+var HEADER = './src/header.js';
+var DIST = './dist';
+
+// generate banner with today's date and correct version
+function createBanner() {
+  var today = gutil.date(new Date(), 'yyyy-mm-dd'); // today, formatted as yyyy-mm-dd
+  var version = require('./package.json').version;  // math.js version
+
+  return String(fs.readFileSync(HEADER))
+    .replace('@@date', today)
+    .replace('@@version', version);
+}
+
+var bannerPlugin = new webpack.BannerPlugin(createBanner(), {
+  entryOnly: true,
+  raw: true
+});
+
+// create a single instance of the compiler to allow caching
+var compiler = webpack({
+  entry: ENTRY,
+  output: {
+    library: 'Form-Kit',
+    libraryTarget: 'umd',
+    path: DIST,
+    filename: NAME + '.js'
+  },
+  plugins: [bannerPlugin],
+  module: {
+    loaders: [
+      {test: /\.json$/, loader: "json"}
+    ]
+  },
+  cache: true
+});
+
+// make dist folder structure
+gulp.task('mkdir', function () {
+  mkdirp.sync(DIST);
+});
+
+// bundle javascript
+gulp.task('bundle', ['mkdir'], function (done) {
+  // update the banner contents (has a date in it which should stay up to date)
+  bannerPlugin.banner = createBanner();
+
+  compiler.run(function (err) {
+    if (err) {
+      gutil.log(err);
+    }
+
+    gutil.log('bundled ' + NAME + '.js');
+
+    done();
+  });
+});
+
+gulp.task('minify', ['bundle'], function () {
+
+  var result = uglify.minify([DIST + '/' + NAME + '.js'], {
+    outSourceMap: NAME + '.map',
+    output: {
+      comments: /@license/,
+      max_line_len: 64000
+    }
+  });
+
+  var fileMin = DIST + '/' + NAME + '.min.js';
+  var fileMap = DIST + '/' + NAME + '.map';
+
+  fs.writeFileSync(fileMin, result.code);
+  fs.writeFileSync(fileMap, result.map);
+
+  gutil.log('Minified ' + fileMin);
+  gutil.log('Mapped ' + fileMap);
+});
+
+gulp.task('watch', ['bundle'], function () {
+  gulp.watch(['src/**/*'], ['bundle', 'minify']);
+});
+
+gulp.task('default', ['bundle', 'minify']);
---
+++ src/form-kit.js
@@ -0,0 +1,131 @@
+jQuery.fn.FormKit = function (args) {
+
+  // return json string
+  if (typeof(args) === 'undefined') {
+    var dataSet = {};
+
+    // make data set object
+    $.each($(this).serializeArray(), function () {
+      if (this.name.match(/^.+\[\]$/)) {
+        // if name end of [] use array logic to process
+        if (typeof(dataSet[this.name]) !== 'object') {
+          dataSet[this.name] = [];
+        }
+        dataSet[this.name].push(this.value);
+      } else {
+        dataSet[this.name] = this.value;
+      }
+    });
+
+    // native json encode
+    if (typeof(JSON) === 'object' && typeof(JSON.stringify) === 'function') {
+      return JSON.stringify(dataSet);
+    } else {
+      return $.toJSON(dataSet);
+    }
+  }
+
+  // set json string to form element
+  var parent = this, setValue, obj = null;
+  if (typeof(args) === 'string') {
+    obj = jQuery.parseJSON(args);
+  } else if (typeof(args) === 'object') {
+    obj = args;
+  }
+  if (obj === null) {
+    console.error('input obj not found.');
+    return;
+  }
+
+  setValue = function (name, value) {
+    if (typeof(value) === 'undefined' || value === null) {
+      return;
+    }
+    var queryElements = $(parent).find(
+      'input[name="' + name + '"], select[name="' + name + '"], textarea[name="' + name + '"]'
+    );
+
+    $.each(queryElements, function (index, element) {
+      var obj = $(element);
+
+      switch (obj.context.type) {
+        case 'text':
+        case 'hidden':
+        case 'email':
+        case 'date':
+        case 'datetime-local':
+        case 'color':
+        case 'month':
+        case 'number':
+        case 'password':
+        case 'search':
+        case 'range':
+        case 'tel':
+        case 'url':
+        case 'time':
+        case 'week':
+        case 'select-one':
+        case 'select':
+          obj.val(value);
+          obj.triggerHandler('change');
+          break;
+        case 'checkbox':
+          obj.each(function () {
+            if (typeof(value) === 'string' || typeof(value) === 'number') {
+              // single
+              if ($(this).val().toString() === value.toString()) {
+                $(this).prop('checked', true);
+              } else {
+                $(this).prop('checked', false);
+              }
+            } else if (typeof(value) === 'object') {
+              // multiple
+              $(this).prop('checked', false);
+              for (var i in value) {
+                if ($(this).val().toString() === value[i].toString()) {
+                  $(this).prop('checked', true);
+                }
+              }
+            }
+          });
+          break;
+        case 'select-multiple':
+          $(this).find('option').each(function () {
+            $(this).prop('selected', false);
+            if (typeof(value) === 'object') {
+              // multiple
+              for (var i in value) {
+                if ($(this).val().toString() === value[i].toString() || $(this).text() === value[i].toString()) {
+                  $(this).prop('selected', true);
+                }
+              }
+            } else if ($(this).val().toString() === value.toString()) {
+              $(this).prop('selected', true);
+            }
+          });
+          break;
+        case 'radio':
+          obj.each(function () {
+            if ($(this).val().toString() === value.toString()) {
+              $(this).prop('checked', true);
+            }
+          });
+          break;
+        case 'textarea':
+          obj.val(value);
+          break;
+        default:
+          console.error('unknown type: ' + obj.context.type);
+          break;
+      }
+    });
+  };
+
+  $.each(obj, function (key, value) {
+    if (value == null) {
+      return;
+    }
+    setValue(key, value);
+  });
+
+};
